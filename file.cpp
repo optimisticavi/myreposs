1d6be9bb20c2ba1b19b13fc2bf75e6a0fdcb4172
optimisticavi <optimisticavi@gmail.com>
2016-03-02 01:04:55 -0800

    committt

--- file.cpp
+++ file.cpp
@@ -5,14 +5,12 @@ int a,b,c;
 a= 2;
 b= 3;
 c = a+b;
-<<<<<<< HEAD
 int d = a*b*c;
-
-=======
 c=b-a;
 d = a+b+c;
 >>>>>>> 773883672e3c3bb8aa5577418bc8f85131f7a8f9
 cout<< added
 cout<<"A";
 cout<<"b";
+cout<<"asdlsjdkjad ";
 }
